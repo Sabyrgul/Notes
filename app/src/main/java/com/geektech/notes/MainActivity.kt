8429b09749d1e12938843c69f1326c1e17789c77
Sabyrgul <quaker.ss11@gmail.com>
2022-12-23 23:29:26 +0600

    Hw 4, ex 2

---
+++ app/src/main/java/com/geektech/notes/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.geektech.notes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var recyclerView: RecyclerView
+    lateinit var editText: EditText
+    lateinit var button: Button
+    lateinit var adapter: NoteAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        recyclerView=findViewById(R.id.main_recycler)
+        editText=findViewById(R.id.edit_note)
+        button=findViewById(R.id.add_note)
+        adapter=NoteAdapter()
+
+        recyclerView.adapter=adapter
+
+        button.setOnClickListener {
+            editText.text.isEmpty()
+            editText.error="Error"
+                adapter.addNote(editText.text.toString())
+        }
+    }
+}
---
+++ app/src/main/java/com/geektech/notes/NoteAdapter.kt
@@ -0,0 +1,38 @@
+package com.geektech.notes
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class NoteAdapter: RecyclerView.Adapter<NoteAdapter.ViewHolder>(){
+
+    var list: MutableList<String> = ArrayList()
+
+    fun addNote(text: String){
+        list.add(text)
+        notifyItemInserted(list.size)
+    }
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        var text=itemView.findViewById<TextView>(R.id.item_text)
+
+        fun bind(message: String){
+           text.text=message
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(parent.context).
+        inflate(R.layout.item_note,parent,false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(list[position])
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+}
